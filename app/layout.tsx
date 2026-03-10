7f8845289777c1c5ad7178a6ab8300dba432bcc9
sam <solopez1018@gmail.com>
2026-03-09 18:20:46 -0700

    fixed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,30 @@ import "@/src/index.css";
 import "@/src/tokens.css";
 
 export const metadata: Metadata = {
-    title: "Portfolio",
-    description: "My Portfolio",
+    metadataBase: new URL("https://samorlopez.com"),
+    title: {
+        default: "Samuel Lopez | Portfolio 2026",
+        template: "%s | Samuel Lopez",
+    },
+    description: "Product design portfolio of Samuel Lopez.",
+    icons: {
+        icon: "/images/favicon.png",
+        apple: "/images/apple_touch_icon.png",
+    },
+    openGraph: {
+        title: "Samuel Lopez | Portfolio 2026",
+        description: "Product design portfolio of Samuel Lopez.",
+        url: "https://samorlopez.com",
+        siteName: "Samuel Lopez Portfolio",
+        images: [
+            {
+                url: "/images/og_image.png",
+                width: 1200,
+                height: 630,
+            },
+        ],
+        type: "website",
+    },
 };
 
 export default function RootLayout({
